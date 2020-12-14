ce7eb52b8cbf270ddf3eb9964e5044ea8aff21e3
ljh468 <ljh468@naver.com>
2020-12-14 23:41:14 +0900

    201214

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("hello")
+
+a = 2
+print(a)
+print(type(a))
+
+b = 3.14
+print(b)
+print(type(b))
+
+print(a*b)
+print(a/b)
+print(b//a)
+b = 8
+a = 3
+print(b%a)
+
+a = "python 's favorite food is perl"
+b = 'python \'s favorite food is perl'
+print(a)
+print(b)
